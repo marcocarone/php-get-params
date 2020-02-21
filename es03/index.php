076496d53da7932104c10cf3536ebb222a0228e8
Marco <marco.carone@libero.it>
2020-02-21 17:20:12 +0100

    up

---
+++ es03/index.php
@@ -0,0 +1,25 @@
+<?php
+// Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi <p>. Ogni punto un nuovo paragrafo.
+$text = "Le stazioni si somigliano tutte; poco importa se le luci non riescono a rischiarare più in là del loro alone sbavato, tanto questo è un ambiente che tu conosci a memoria, con l'odore di treno che resta anche dopo che tutti i treni sono partiti, l'odore speciale delle stazioni dopo che è partito l'ultimo treno. Le luci della stazione e le frasi che stai leggendo sembra abbiano il compito di dissolvere più che di indicare le cose affioranti da un velo di buio e di nebbia. Io sono sbarcato in questa stazione stasera per la prima volta in vita mia e già mi sembra d'averci passato una vita, entrando e uscendo da questo bar, passando dall'odore della pensilina all'odore di segatura bagnata dei gabinetti, tutto mescolato in un unico odore che è quello dell'attesa, l'odore delle cabine telefoniche quando non resta che recuperare i gettoni perché il numero chiamato non dà segno di vita. ";
+
+$paragrafi = explode(". ", $text);
+// var_dump($paragrafi);
+ ?>
+
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <div class="">
+       <?php
+          for ($i=0; $i < count($paragrafi); $i++) {
+            echo "<p> $paragrafi[$i] </p>";
+          }
+        ?>
+     </div>
+   </body>
+ </html>
